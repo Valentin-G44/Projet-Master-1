3dbc2030b36cfab4f7945c93b37138f2e2ad093b
Valentin G <126241616+Valentin-G44@users.noreply.github.com>
2024-03-28 16:05:57 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,21 @@ echo "Installation de Nmap..."
 sudo apt update
 sudo apt install -y nmap
 
+# Installation de Curl avec sudo
+echo "Installation de Curl..."
+sudo apt install -y curl
+
+# Installation de Dirb avec sudo
+echo "Installation de Dirb..."
+sudo apt install -y dirb
+
+# Installation de Nikto avec sudo
+echo "Installation de Nikto..."
+sudo apt install -y nikto
+
 # Installation de Hydra avec sudo
 echo "Installation de Hydra..."
 sudo apt install -y hydra
 
-# Installation de Python3
-echo "Installation de Python3..."
-apt-get install python3 -y
+
+
